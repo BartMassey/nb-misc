5a71d439130ae52d3d7bed8e735d950eacd2e5f9
Bart Massey <bart@cs.pdx.edu>
2014-07-28 13:21:36 -0700

    added die-roller app

---
+++ die-roller.py
@@ -0,0 +1,27 @@
+# Copyright © 2014 Bart Massey
+# Display a die roll
+
+from random import *
+from tkinter import *
+
+class DieRoller(Frame):
+
+    def __init__(self, root):
+        super().__init__(root)
+        self.grid()
+        self.display = Label(self, width = 1)
+        self.display.grid(row = 0, column = 0, sticky = E + W)
+        self.roll = Button(self, text = "Roll", command = self.do_roll)
+        self.roll.grid(row = 1, column = 0, sticky = E + W)
+        
+    def do_roll(self):
+        pass
+
+# Seed the PRNG.
+seed()
+
+# Start the app.
+root = Tk()
+root.title("Die Roller")
+_ = DieRoller(root)
+root.mainloop()
