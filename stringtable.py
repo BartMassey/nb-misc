d5c5304d6e47bdb138686b2bdbc224dda9ef6881
Bart Massey <bart@cs.pdx.edu>
2014-08-14 13:05:42 -0700

    added stringtable tests

--- stringtable.py
+++ stringtable.py
@@ -3,12 +3,46 @@
 
 from djb2 import djb2
 
-def StringTable(object):
+class StringTable(object):
     def __init__(self):
-        self.table = [None]*65536
+        self.table = [[]]*65536
 
+    # Return the value at position k.
     def lookup(self, k):
-        pass
+        return None
 
+    # Insert the value v at position k,
+    # overwriting any existing value at k.
     def insert(self, k, v):
         pass
+
+if __name__ == "__main__":
+    from random import randrange
+
+    def test(n):
+
+        def randstring():
+            r = ""
+            for _ in range(randrange(5)):
+                r += chr(randrange(32, 128))
+            return r
+
+        t = StringTable()
+        entries = {}        
+        for _ in range(n):
+            k = randstring()
+            v = randrange(1000)
+            t.insert(k, v)
+            entries[k] = v
+        for k in entries:
+            assert entries[k] == t.lookup(k)
+        for _ in range(n):
+            k = randstring()
+            if k in entries:
+                continue
+            assert t.lookup(k) == None
+        print(".", end="")
+
+    print("random testing")
+    for _ in range(10):
+        test(64000)
