747e541595d23d2b5470c580cb7e5b398884beda
Bart Massey <bart@cs.pdx.edu>
2014-08-18 13:30:56 -0700

    added way more methods to tree

--- tree.py
+++ tree.py
@@ -53,5 +53,64 @@ class Tree(object):
 
         return self.postorder(op)
 
+    def frontier(self):
+        def maybe_traverse(t):
+            if t == None:
+                return []
+            else:
+                return t.frontier()
+
+        if self.left == None and self.right == None:
+            return [self.label]
+        l = maybe_traverse(self.left)
+        r = maybe_traverse(self.right)
+        return l + r
+
+    def _frontier(self):
+        if self.left == None and self.right == None:
+            return [self.label]
+        elif self.left == None:
+            return self.right._frontier()
+        elif self.right == None:
+            return self.left._frontier()
+        else:
+            return self.left._frontier() + self.right._frontier()
+
+    def widths(self):
+        def zip(left, right):
+            # If one of the lists is empty, there's
+            # nothing to add, so return the other one.
+            if left == []:
+                return right
+            if right == []:
+                return left
+            # Return the sum of the first elements with
+            # the zip of the rest tacked on.
+            return [left[0] + right[0]] + zip(left[1:], right[1:])
+
+        def _zip(left, right):
+            # * start with an empty result list
+            result = []
+            # * as long as there's elements on both sides,
+            #   tack their sum onto the result list.
+            common = min(length(left), length(right))
+            for i in range(common):
+                result.append(left[i] + right[i])
+            # * tack on whatever's left over from one of the lists
+            return result + left[common:] + right[common:]
+            
+
+        if self.left == None and self.right == None:
+            return [1]
+        elif self.left == None:
+            return [1] + self.right.widths()
+        elif self.right == None:
+            return [1] + self.left.widths()
+        else:
+            return [1] + zip(self.left.widths(), self.right.widths())
+
+    def width(self):
+        return max(self.widths())
+
 demo_tree = Tree(1, Tree(2, Tree(4, None, Tree(5))), Tree(3))
                                                                                                             
