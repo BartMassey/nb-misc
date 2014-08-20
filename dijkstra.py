18da586a644ac685808ffc2a4fedaf1a8811ab5d
Bart Massey <bart@cs.pdx.edu>
2014-08-20 16:14:05 -0700

    fixed a number of bugs in heap and dijkstra

--- dijkstra.py
+++ dijkstra.py
@@ -34,10 +34,10 @@ def dijkstra(source, graph, dest=None):
             self.distance = distance
             self.back = back
 
-    def ordered(v1, v2):
-        return v1.distance <= v2.distance
+    def comparator(v):
+        return v.distance
     
-    frontier = heap.Heap(heap.minheap, len(graph.edges), compare=ordered)
+    frontier = heap.Heap(heap.minheap, len(graph.edges)**2, compare=comparator)
     frontier.insert(V(source, 0, None))
     while not frontier.is_empty():
         target_v = frontier.extract()
@@ -47,16 +47,42 @@ def dijkstra(source, graph, dest=None):
         if target == dest:
             return
         target.set_mark()
-        for e in graph.edges:
-            if target not in e:
+        for edge in graph.edges:
+            if target not in edge:
                 continue
-            e = next_edge.copy()
-            v = e.pop()
+            (v, v2) = edge
             if v == target:
-                v = e.pop()
-            implied_distance = target.distance + \
-              graph.weights[tuple({target, v})]
+                v = v2
+            implied_distance = target.distance + graph.weights[edge]
             if v.is_marked():
                 assert v.distance <= implied_distance
                 continue
             frontier.insert(V(v, implied_distance, target))
+
+if __name__ == "__main__":
+    es = {}
+    vs = set()
+    for line in open("oregon-mileage-map.txt", "r"):
+        (v1, v2, w) = line.split()
+        vs |= {v1}
+        vs |= {v2}
+        es[tuple({v1, v2})] = int(w)
+    vd = {v : Node(v) for v in vs}
+    gns = set(vd.values())
+    ges = set()
+    gws = {}
+    for e in es:
+        (v1, v2) = e
+        xe = tuple({vd[v1], vd[v2]})
+        ges |= {xe}
+        gws[xe] = es[e]
+    g = Graph(gns, ges, gws)
+    dijkstra(vd["Portland"], g)
+    for v in vs:
+        if v == "Portland":
+            continue
+        vv = vd[v]
+        if not vv.is_marked():
+            print("unvisited", v)
+            continue
+        print("%s: next=%s  dist=%s" % (v, vv.back.label, vv.distance))
