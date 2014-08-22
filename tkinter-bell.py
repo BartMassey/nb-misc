f3da4cf0cc0bb304c17dd0f163b66f965747ffde
Bart Massey <bart@cs.pdx.edu>
2014-08-22 09:57:55 -0700

    cleaned up tkinter-bell a bit

--- tkinter-bell.py
+++ tkinter-bell.py
@@ -6,13 +6,12 @@ from tkinter import *
 class Bell(Frame):
     def __init__(self, root):
         super().__init__(root)
-        self.root = root
         self.pack()
         self.button = Button(self, text = "Bell", command = self.do_bell)
         self.button.pack()
 
     def do_bell(self):
-        root.bell()
+        self.bell()
     
 
 # Start the app.
@@ -20,4 +19,3 @@ root = Tk()
 root.title("Socrate")
 app = Bell(root)
 root.mainloop()
-app.close()
