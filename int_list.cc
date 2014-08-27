1eb1f03f3744938828aecc3e2a544fce3d24ad17
Bart Massey <bart@cs.pdx.edu>
2014-08-27 11:43:40 -0700

    renamed int_list and demod name mangling

--- int_list.cc
+++ int_list.cc
@@ -2,6 +2,9 @@
 /* intlist demo */
 
 #include <stdio.h>
+extern "C" {
+#include "intlist.h"
+}
 
 class int_node {
 public:
@@ -32,17 +35,52 @@ public:
     }
 
     virtual void print(void) {
-        int_node *lp;
-        for (lp = this->head; lp; lp = lp->next)
+        for (int_node *lp = this->head; lp; lp = lp->next)
             lp->print();
     }
 };
 
+class int_list_adapter {
+    struct intlist *head;
+
+public:
+    int_list_adapter(void) {
+        head = 0;
+    }
+
+    virtual void prepend(int v) {
+        intlist_prepend(v, &head);
+    }
+
+    virtual void append(int v) {
+        intlist_append(v, &head);
+    }
+
+    ~int_list_adapter(void) {
+        intlist_free(&head);
+    }
+
+    virtual void print(void) {
+        intlist_print(&head);
+    }
+
+    virtual int extract_min(void) {
+        return intlist_extract_min(&head);
+    }
+};
+
 int main(void) {
-    int i;
     int_list l;
-    for (i = 0; i < 10; i++)
+    for (int i = 0; i < 10; i++)
         l.prepend(i);
     l.print();
+
+    printf("\n");
+
+    int_list_adapter al;
+    for (int i = 0; i < 10; i++)
+        al.prepend(i);
+    al.print();
+
     return 0;
 }
