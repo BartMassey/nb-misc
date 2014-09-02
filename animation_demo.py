a48cf1b0031a101028d96dc1db8790fdd87590ba
Bart Massey <bart@cs.pdx.edu>
2014-09-02 10:50:40 -0700

    updated animation demo

---
+++ animation_demo.py
@@ -0,0 +1,32 @@
+# Copyright © 2014 Bart Massey
+# ASCII CR animation demo.
+
+from time import sleep
+
+width = 20
+
+def clear():
+    print(end="\r")
+    print(" " * width, end="\r")
+
+def display_animation(figure, positions):
+
+    def display_frame(i):
+        print(" " * i + figure, end="\r")
+
+    for i in positions:
+        sleep(1/20)
+        clear()
+        display_frame(i)
+
+def animation_move_right(figure):
+    display_animation(figure, range(width - len(figure) + 1))
+
+def animation_move_left(figure):
+    display_animation(figure, range(width - len(figure), -1, -1))
+
+animation_move_right("->")
+animation_move_left("<-")
+animation_move_right("=>")
+animation_move_left("<=")
+print()
